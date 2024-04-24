7d34454fdd4a6c1cd12d501defc39b3f383e6b7b
elhanan.p <elhanan.p@sps.co.il>
2024-04-24 09:52:06 +0300

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  RouterFeatures,
+  withComponentInputBinding,
+  withEnabledBlockingInitialNavigation,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+const routerProviderFeatures: RouterFeatures[] = [
+  withEnabledBlockingInitialNavigation(),
+  withComponentInputBinding(),
+  withViewTransitions(),
+];
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, ...routerProviderFeatures),
+    provideAnimationsAsync(), provideAnimationsAsync(),
+  ],
 };
